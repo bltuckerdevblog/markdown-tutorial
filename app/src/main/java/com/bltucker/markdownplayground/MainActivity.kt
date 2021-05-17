93f400e8db4ca4f4e49b86f570478c2c2bcc9fa1
Brett Tucker <bretttucker@gmail.com>
2021-05-16 19:29:06 -0600

    Add initial project files

---
+++ app/src/main/java/com/bltucker/markdownplayground/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bltucker.markdownplayground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
